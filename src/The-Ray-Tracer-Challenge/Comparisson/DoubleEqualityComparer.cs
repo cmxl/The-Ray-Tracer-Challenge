413a37797fe715c816669c3d21cbb2d1c516671a
Tobias Haimerl <th.cmxl@gmail.com>
2019-07-02 01:16:50 +0200

    default implementation of the equality comparer fixed

--- src/The-Ray-Tracer-Challenge/Comparisson/DoubleEqualityComparer.cs
+++ src/The-Ray-Tracer-Challenge/Comparisson/DoubleEqualityComparer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using The_Ray_Tracer_Challenge.Extensions;
 
 namespace The_Ray_Tracer_Challenge.Comparisson
 {
@@ -9,16 +8,18 @@ namespace The_Ray_Tracer_Challenge.Comparisson
         public static readonly double Delta = 0.000001;
         public static readonly int MaxDecimals = 5;
 
-        public static readonly IEqualityComparer<double> Default = new DoubleEqualityComparer(Delta);
+        public static readonly IEqualityComparer<double> Default = new DoubleEqualityComparer(Delta, MaxDecimals);
 
         private readonly double _delta;
+        private readonly int _decimals;
 
-        public DoubleEqualityComparer(double delta)
+        public DoubleEqualityComparer(double delta, int decimals)
         {
             _delta = delta;
+            _decimals = decimals;
         }
 
-        public bool Equals(double x, double y) => Math.Abs(Math.Round(x, MaxDecimals) - Math.Round(y, MaxDecimals)) <= Delta;
+        public bool Equals(double x, double y) => Math.Abs(Math.Round(x, _decimals) - Math.Round(y, _decimals)) <= _delta;
 
         public int GetHashCode(double obj) => HashCode.Combine(obj);
     }
